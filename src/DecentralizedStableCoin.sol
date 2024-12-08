5305e4ddcc0e1a0b9d99e3a1a297ad2fa4de431a
0xRudeboy <rudeboyweb3@gmail.com>
2024-12-09 00:29:01 +0400

    write stablecoin ERC20Burnable smart contract

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.18;
 
 import {ERC20, ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title DecentralizedStableCoin
@@ -13,6 +14,41 @@ import {ERC20, ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensio
  * This is the contract meant to be governed by the DSCEngine. This contract is just the ERC20
  implementation of our stablecoin system.
  */
-contract DecentralizedStableCoin is ERC20Burnable {
-    constructor() ERC20("DecentralizedStableCoin", "DSC") {}
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
+
+    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(msg.sender) {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+
+        if (balance < _amount) {
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+
+        // super keyword is used to call the burn function from the parent class ERC20Burnable (because this function is also named burn)
+        super.burn(_amount);
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+
+        _mint(_to, _amount);
+        return true;
+    }
 }
